ee3780311d9f78733249f90061b9cec12705168f
Kevin von Flotow <vonflow@gmail.com>
2014-01-07 12:49:19 -0600

    changed prototype format

--- nodehelper.js
+++ nodehelper.js
@@ -7,119 +7,120 @@
 		this.logging = true;
 	}
 
-	nh.prototype.set = function ( key, val ) {
-		this[ key ] = val;
-	}
+	nh.prototype = {
+		set: function ( key, val ) {
+			this[ key ] = val;
+		},
 
-	nh.prototype.nhLog = function ( msg ) {
-		return this.logging ? console.log( 'nodehelper: ' + msg.toString() ) : false;
-	}
+		nhLog: function ( msg ) {
+			return this.logging ? console.log( 'nodehelper: ' + msg.toString() ) : false;
+		},
 
-	nh.prototype.isString = function( str ) {
-		return typeof str === 'string' || str instanceof String;
-	};
+		isString: function ( str ) {
+			return typeof str === 'string' || str instanceof String;
+		},
 
-	// http://dl.dropboxusercontent.com/u/35146/js/tests/isNumber.html
-	nh.prototype.isNumber = function ( num ) {
-		return !isNaN( parseFloat( num ) ) && isFinite( num );
-	};
+		// http://dl.dropboxusercontent.com/u/35146/js/tests/isNumber.html
+		isNumber: function ( num ) {
+			return !isNaN( parseFloat( num ) ) && isFinite( num );
+		},
 
-	nh.prototype.isObject = function( obj ) {
-		return obj === Object( obj ) && Object.prototype.toString.call( obj ) !== '[object Array]';
-	};
+		isObject: function ( obj ) {
+			return obj === Object( obj ) && Object.prototype.toString.call( obj ) !== '[object Array]';
+		},
 
-	nh.prototype.each = function ( obj, callback ) {
-		if ( obj.isArray ) {
-			// don't see the need for an isArray fallback in node
-			// loops a normal array - []
+		each: function ( obj, callback ) {
+			if ( obj.isArray ) {
+				// don't see the need for an isArray fallback in node
+				// loops a normal array - []
 
-			for ( var i = 0, l = obj.length; i < l; i++ )
-				callback.call( obj, i, obj[ i ] );
+				for ( var i = 0, l = obj.length; i < l; i++ )
+					callback.call( obj, i, obj[ i ] );
 
-		} else if ( obj === Object( obj ) ) {
-			// loops an object - {}
+			} else if ( obj === Object( obj ) ) {
+				// loops an object - {}
 
-			for ( var key in obj )
-				callback.call( obj, key, obj[ key ] );
+				for ( var key in obj )
+					callback.call( obj, key, obj[ key ] );
 
-		} else if ( this.isNumber( obj ) ) {
-			// loops a number - 23
+			} else if ( this.isNumber( obj ) ) {
+				// loops a number - 23
 
-			for ( var i = 0; i < obj; i++ )
-				callback.call( obj, i );
+				for ( var i = 0; i < obj; i++ )
+					callback.call( obj, i );
 
-		} else if ( this.isString( obj ) ) {
-			// loops a string - 'foo'
+			} else if ( this.isString( obj ) ) {
+				// loops a string - 'foo'
 
-			for ( var i = 0, l = obj.length; i < l; i++ )
-				callback.call( obj, obj.charAt( i ) );
-		} else {
-			this.nhLog( 'each: invalid type' );
+				for ( var i = 0, l = obj.length; i < l; i++ )
+					callback.call( obj, obj.charAt( i ) );
+			} else {
+				this.nhLog( 'each: invalid type' );
+			}
+		},
+
+		// provide some bitwise math alternatives
+		bitFloor: function ( num ) {
+			return num | 0;
+		},
+
+		bitCeil: function ( num ) {
+			return ( num + 1 ) | 0;
+		},
+
+		bitRound: function ( num ) {
+			return ( num + 0.5 ) | 0;
+		},
+
+		// lerp function, not 100% sure this is useful for nodejs - possibly smoothing out test results
+		lerp: function ( fromNum, toNum, t ) {
+			return fromNum + ( toNum - fromNum ) * t;
+		},
+
+		// Fisher-Yates shuffle
+		shuffle: function ( arr ) {
+			if ( !arr || !arr.isArray )
+				return false;
+
+			var n = arr.slice( 0 ),
+				i = n.length,
+				j, t;
+
+			while ( --i > 0 ) {
+				j = ( Math.random() * ( i + 1 ) ) | 0;
+				t = n[ j ];
+				n[ j ] = n[ i ];
+				n[ i ] = t;
+			}
+
+			return n;
+		},
+
+		// setting shuffleOverride to true disables the shuffle - useful for one-off randoms where performance is a concern
+		randomFromArray: function ( arr, shuffleOverride ) {
+			if ( !arr || !arr.isArray )
+				return false;
+
+			// shuffle the array before returning to make it really random
+			if ( !shuffleOverride )
+				arr = this.shuffle( arr );
+
+			return arr[ ( Math.random() * arr.length ) | 0 ];
+		},
+
+		// using with one argument should just make a clone of the object
+		extend: function () {
+			var a = arguments;
+
+			if ( a.length === 0 )
+				return this.nhLog( 'extend requires at least 1 argument' );
+
+			for ( var i = 0, l = a.length, n = Object.create( a[ 0 ] ); i < l; i++ )
+				for ( var obj in a[ i ] )
+					n[ obj ] = a[ i ][ obj ];
+
+			return n;
 		}
-	};
-
-	// provide some bitwise math alternatives
-
-	nh.prototype.bitFloor = function ( num ) {
-		return num | 0;
-	};
-
-	nh.prototype.bitCeil = function ( num ) {
-		return ( num + 1 ) | 0;
-	};
-
-	nh.prototype.bitRound = function ( num ) {
-		return ( num + 0.5 ) | 0;
-	};
-
-	// lerp function, not 100% sure this is useful for nodejs - possibly smoothing out test results
-	nh.prototype.lerp = function ( fromNum, toNum, t ) {
-		return fromNum + ( toNum - fromNum ) * t;
-	};
-
-	// Fisher-Yates shuffle
-	nh.prototype.shuffle = function ( arr ) {
-		if ( !arr || !arr.isArray )
-			return false;
-
-		var n = arr.slice( 0 ),
-			i = n.length,
-			j, t;
-
-		while ( --i > 0 ) {
-			j = ( Math.random() * ( i + 1 ) ) | 0;
-			t = n[ j ];
-			n[ j ] = n[ i ];
-			n[ i ] = t;
-		}
-
-		return n;
-	};
-
-	// setting shuffleOverride to true disables the shuffle - useful for one-off randoms where performance is a concern
-	nh.prototype.randomFromArray = function ( arr, shuffleOverride ) {
-		if ( !arr || !arr.isArray )
-			return false;
-
-		// shuffle the array before returning to make it really random
-		if ( !shuffleOverride )
-			arr = this.shuffle( arr );
-
-		return arr[ ( Math.random() * arr.length ) | 0 ];
-	};
-
-	// using with one argument should just make a clone of the object
-	nh.prototype.extend = function () {
-		var a = arguments;
-
-		if ( a.length === 0 )
-			return this.nhLog( 'extend requires at least 1 argument' );
-
-		for ( var i = 0, l = a.length, n = Object.create( a[ 0 ] ); i < l; i++ )
-			for ( var obj in a[ i ] )
-				n[ obj ] = a[ i ][ obj ];
-
-		return n;
 	};
 
 	module.exports = new nh();
